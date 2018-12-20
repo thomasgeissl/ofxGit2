784fe2df99efd1828ec5e4df52d5495abdf5f6d9
Thomas Geissl <thomas.geissl@gmail.com>
2018-12-19 23:33:41 -0500

    added addon config

---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxGit2
+	ADDON_DESCRIPTION = Addon for working with git repositories
+	ADDON_AUTHOR = Thomas Geissl
+	ADDON_TAGS = "git" "libgit2"
+	ADDON_URL = https://github.com/thomasgeissl/ofxGit2
+
+common:
+	ADDON_PKG_CONFIG_LIBRARIES = libgit2
