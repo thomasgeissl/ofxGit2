3a85b8f323e258f985f2d938ba18e84bbc4e010c
Thomas Geissl <thomas.geissl@gmail.com>
2019-12-10 13:05:31 +0100

    fixed check for commit

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -17,7 +17,7 @@ int main()
     ofLogNotice("main") << "f1d92fd8e65f97383fcdc6612dd6b92e3a37e7e0 is commit " << repo.isCommit("f1d92fd8e65f97383fcdc6612dd6b92e3a37e7e0");
     ofLogNotice("main") << "-1 is commit " << repo.isCommit("-1");
     ofLogNotice("main") << "1.1.1 is commit " << repo.isCommit("1.1.1");
-    ofLogNotice("main") << "2f0e6343 (tag 1.1.1) is commit " << repo.isTag("2f0e6343");
+    ofLogNotice("main") << "2f0e6343 (tag 1.1.1) is commit " << repo.isCommit("2f0e6343");
     ofLogNotice("main") << "1.1.1 is tag " << repo.isTag("1.1.1");
     ofLogNotice("main") << "-1 is tag " << repo.isTag("-1");
     ofLogNotice("main") << "successfully checked out commit 2f0e6343c817a4f5a33b60339c82b5d10be8af01: " << repo.checkoutCommit("2f0e6343c817a4f5a33b60339c82b5d10be8af01");
